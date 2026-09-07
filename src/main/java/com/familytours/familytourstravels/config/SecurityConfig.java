89d57c8f9ae377713c36e9359c75bc06975eefc2
ramakrishnareddy <sankarreddysyamala@gmail.com>
2026-09-07 17:48:11 +0530

    Fix Spring Security CORS configuration

--- src/main/java/com/familytours/familytourstravels/config/SecurityConfig.java
+++ src/main/java/com/familytours/familytourstravels/config/SecurityConfig.java
@@ -29,8 +29,7 @@ public class SecurityConfig {
 
         http
         .csrf(csrf -> csrf.disable())
-
-        .cors(cors -> {})
+        .cors(cors -> cors.disable())
 
             .sessionManagement(session ->
                 session.sessionCreationPolicy(
